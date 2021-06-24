b445429f6aeca6558d889facb5b3200367749060
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-24 22:06:04 +0530

    React: Use CSS Styling - variable

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+const h2Styles = { color: "crimson", backgroundColor: "#eee", padding: "20px" };
+
 export default class Display extends Component {
     render() {
         return (
             <div>
-                <h2 style={{ color: "crimson", backgroundColor: "#eee", padding: "20px" }}>{this.props.name}</h2>
+                <h2 style={h2Styles}>{this.props.name}</h2>
             </div>
         )
     }

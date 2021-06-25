bbac72fa1c0af6a665d4507cf7c1136cb2670141
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-25 16:02:30 +0530

    React: filter()

--- src/App.js
+++ src/App.js
@@ -2,50 +2,13 @@ import './App.css';
 import React from 'react'
 
 const App = () => {
-  const colors = [
-    {
-      id: 1,
-      title: 'White'
-    },
+  const colors = ['White', 'Yellow', 'Blue', 'Red', 'Green', 'Black', 'Brown'];
+  const filterColor = colors.filter(color => color.includes('B'));
 
-
-    {
-      id: 2,
-      title: 'Yellow'
-    },
-
-
-    {
-      id: 3,
-      title: 'Blue'
-    },
-
-
-    {
-      id: 4,
-      title: 'Red'
-    },
-
-
-    {
-      id: 5,
-      title: 'Green'
-    },
-
-
-    {
-      id: 6,
-      title: 'Black'
-    },
-    {
-      id: 7,
-      title: 'Brown'
-    }
-  ]
   return (
     <div>
       <h2>colors</h2>
-      {colors.map((value) => <li key={value.id}>{value.title}</li>)}
+      {filterColor.map(item => <li key={item}>{item}</li>)}
     </div>
   )
 }

37aabda819f14240a1f8d2e5e99a6b1e57e4d5bb
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-25 15:54:25 +0530

    React: map() - Object

--- src/App.js
+++ src/App.js
@@ -2,11 +2,50 @@ import './App.css';
 import React from 'react'
 
 const App = () => {
-  const colors = ['White', 'Yellow', 'Blue', 'Red', 'Green', 'Black', 'Brown']
+  const colors = [
+    {
+      id: 1,
+      title: 'White'
+    },
+
+
+    {
+      id: 2,
+      title: 'Yellow'
+    },
+
+
+    {
+      id: 3,
+      title: 'Blue'
+    },
+
+
+    {
+      id: 4,
+      title: 'Red'
+    },
+
+
+    {
+      id: 5,
+      title: 'Green'
+    },
+
+
+    {
+      id: 6,
+      title: 'Black'
+    },
+    {
+      id: 7,
+      title: 'Brown'
+    }
+  ]
   return (
     <div>
-      <h2>Colors</h2>
-      {colors.map((value, index) => <li key={index}>{value}</li>)}
+      <h2>colors</h2>
+      {colors.map((value) => <li key={value.id}>{value.title}</li>)}
     </div>
   )
 }

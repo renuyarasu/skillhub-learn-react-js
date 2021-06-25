a9a6f13c3d7cfb1b703fccc7de4e63af42227cc2
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-25 08:05:44 +0530

    React: use useEffect - counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  useEffect(() => console.log(counter), [counter])
   return (
     <div>
       <center>
-        <h2>{counter}</h2>
-        <button onClick={() => setCounter(counter+1)}>Clicked</button>
+        <h2>You clicked {counter} times.</h2>
+        <button onClick={() => setCounter(counter + 1)}>+1</button>
       </center>
     </div>
   )

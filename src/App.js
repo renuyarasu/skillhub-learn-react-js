eb6ddfc5fd8b4f31bafe31e0ae8276ea99b39c75
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-30 21:47:50 +0530

    Create Sign up Form - Validations

--- src/App.js
+++ src/App.js
@@ -15,21 +15,23 @@ const App = () => {
   }
   const submitHandler = e => {
     e.preventDefault();
-    if (password === passwordConfirm) {
+    if (username.length <= 5) {
+      alert('Username must be at 5 characters!')
+    } else if (password !== passwordConfirm) {
+      alert('Passwords are not matching!')
+    }else{
       console.log(data);
-    } else {
-      console.log('Passwords are not matching!');
     }
   }
   return (
     <div>
       <h1>Contact Form</h1>
       <form onSubmit={submitHandler}>
-        <input type='username' name='username' placeholder='username' onChange={changeHandler} /> <br /><br />
-        <input type='email' name='email' placeholder='email' onChange={changeHandler} /> <br /><br />
-        <input type='password' name='password' placeholder='password' onChange={changeHandler} /> <br /><br />
-        <input type='password' name='passwordConfirm' placeholder='passwordConfirm' onChange={changeHandler} /> <br />
-        <br />
+        <input type='username' name='username' placeholder='username' onChange={changeHandler} /> <br />
+        <input type='email' name='email' placeholder='email' onChange={changeHandler} /> <br />
+        <input type='password' name='password' placeholder='password' onChange={changeHandler} /> <br />
+        <input type='password' name='passwordConfirm' placeholder='Confirm password' onChange={changeHandler} /> <br />
+        {password !== passwordConfirm ? <p style={{color:'crimson', fontWeight:700}}>Passwords are not matching!</p> : null}
         <input type='submit' value='submit' />
       </form>
     </div>

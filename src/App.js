5b857285f666136d872a83a6de6cb71660cfe2fa
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-30 21:33:02 +0530

    Create Sign up Form - passwordConfirm

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import React, { useState } from 'react'
 const App = () => {
   const [data, setData] = useState({
     username: '',
+    email: '',
     password: '',
+    passwordConfirm: '',
   })
-  const { username, password } = data;
+  const { username, email, password, passwordConfirm } = data;
 
   const changeHandler = e => {
-    setData({ ...data, [e.target.name]: [e.target.value] })
+    setData({ ...data, [e.target.name]: e.target.value })
   }
   const submitHandler = e => {
     e.preventDefault();
-    console.log(data);
+    if (password === passwordConfirm) {
+      console.log(data);
+    } else {
+      console.log('Passwords are not matching!');
+    }
   }
-
   return (
     <div>
       <h1>Contact Form</h1>
       <form onSubmit={submitHandler}>
         <input type='username' name='username' placeholder='username' onChange={changeHandler} /> <br /><br />
-        <input type='password' name='password' placeholder='password' onChange={changeHandler} /> <br />
+        <input type='email' name='email' placeholder='email' onChange={changeHandler} /> <br /><br />
+        <input type='password' name='password' placeholder='password' onChange={changeHandler} /> <br /><br />
+        <input type='password' name='passwordConfirm' placeholder='passwordConfirm' onChange={changeHandler} /> <br />
         <br />
         <input type='submit' value='submit' />
       </form>

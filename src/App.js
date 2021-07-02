cd58711732da9499f25f0b68861646ebefe94e0e
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-07-02 17:17:11 +0530

    Import Video

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
 import React from 'react';
+
+// Import Images
 // import FrontEndDevelopment from './FrontEndDevelopment.jpg';
 // import BackEndDeveloper from './BackEndDeveloper.jpg';
 // import FullStackDeveloper from './FullStackDeveloper.jpg';
 
-import audio from './sathamanambhavatiNanna.ogg';
+// Import Audio File
+// import audio from './sathamanambhavatiNanna.ogg';
 
+// Import Video File
+import video from './video.mp4';
 
 const App = () => {
   return (
     <div>
-      <h1>Import Audio File</h1>
+      <h1>Import Video</h1>
       {/* <img src={FrontEndDevelopment} height='250' width='auto' />
       <img src={BackEndDeveloper} height='250' width='auto' />
       <img src={FullStackDeveloper} height='250' width='auto' /> */}
-     <audio controls>
+      {/*  <audio controls>
        <source src={audio} type='audio/ogg'/>
-     </audio>
+     </audio> */}
+  <video width='400' height='auto' controls>
+    <source src={video} type='video/mp4' w />
+  </video>
     </div>
   )
 }

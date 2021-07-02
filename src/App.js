149db60b40b39ec063c1a10cde6581395b81b300
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-07-02 17:13:48 +0530

    Import Audio File

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import React from 'react';
-import FrontEndDevelopment from './FrontEndDevelopment.jpg';
-import BackEndDeveloper from './BackEndDeveloper.jpg';
-import FullStackDeveloper from './FullStackDeveloper.jpg';
+// import FrontEndDevelopment from './FrontEndDevelopment.jpg';
+// import BackEndDeveloper from './BackEndDeveloper.jpg';
+// import FullStackDeveloper from './FullStackDeveloper.jpg';
+
+import audio from './sathamanambhavatiNanna.ogg';
 
 
 const App = () => {
   return (
     <div>
-      <h1>Import Images</h1>
-      <img src={FrontEndDevelopment} height='250' width='auto' />
+      <h1>Import Audio File</h1>
+      {/* <img src={FrontEndDevelopment} height='250' width='auto' />
       <img src={BackEndDeveloper} height='250' width='auto' />
-      <img src={FullStackDeveloper} height='250' width='auto' />
+      <img src={FullStackDeveloper} height='250' width='auto' /> */}
+     <audio controls>
+       <source src={audio} type='audio/ogg'/>
+     </audio>
     </div>
   )
 }

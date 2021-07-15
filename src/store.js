aac4044bb962d8fba3fbf9ade2dc9d58d8492ffd
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-07-15 11:20:24 +0530

    React-Redux: create Store

--- src/store.js
+++ src/store.js
@@ -1,27 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore,applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+
 
 const middleware = [thunk];
 
 const store = createStore(
     reducer,
-
     composeWithDevTools(applyMiddleware(...middleware))
-);
 
+)
 export default store;
-
-
-
-
-
-
-
-
-
-
-
-
-
-

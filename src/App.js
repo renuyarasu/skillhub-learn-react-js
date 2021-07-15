b505ffbfba147a0041d411051dad77de31ebf856
renukaprasad <renukaprasad.yarasu@innroad.com>
2021-07-16 04:11:00 +0530

    Class Component: Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 import './App.css';
-import React, { Component } from 'react';
-import Display from './Display';
+import React from 'react'
 
-export default class App extends Component {
-    state = {
-        name: 'Hello React'
+class Container extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: true,
+            count: 0
+        };
     }
+    delHeader = () => {
+        this.setState({ show: false });
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount Method!');
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate Method!');
+    }
+
     render() {
+        let myheader;
+        if (this.state.show) {
+            myheader = <Child />;
+        };
         return (
             <div>
-                <Display data={this.state.name} />
-                <button onClick={() => this.setState({ name: 'Learning React JS' })}>Change Name</button>
+                <center>
+                    {myheader}
+                    <button type='button' onClick={this.delHeader}>Delete Header</button><hr /><br /><hr />
+                    <h3>Count: {this.state.count}</h3>
+                    <button type='button' onClick={() => this.setState({ count: this.state.count + 1 })}>Increment</button>
+                </center>
             </div>
         )
     }
 }
+class Child extends React.Component {
+    componentWillUnmount() {
+        console.log('componentWillUnmount Method!');
+    }
+    render() {
+        return (
+            <h1>Hello React</h1>
+        );
+    }
+}
+
+export default Container;
+

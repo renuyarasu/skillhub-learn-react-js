79e9e9477b4b97fc77c29481904d6f686dce412e
renukaprasad <renukaprasad.yarasu@innroad.com>
2021-07-16 11:40:15 +0530

    Enter your City Name:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import React from 'react';
-import ContactUs from './ContactUs';
+import React, { useState } from 'react'
+import Data from './city.json';
 
 const App = () => {
+    const [search, setSearch] = useState('');
     return (
         <div>
-            <h1>Email JS</h1>
-            <ContactUs />
+            <h1>Enter your City Name:</h1>
+            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} /> <br /><br />
+            {Data.filter(city => city.name.toLowerCase().includes(search.toLowerCase())).map(city => {
+                return <div className='cities'>
+                    {city.name}
+                </div>
+            })}
         </div>
     )
 }

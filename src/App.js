2466b8e298206853294f3bf250d63412f76532b8
renukaprasad <renukaprasad.yarasu@innroad.com>
2021-07-20 14:30:51 +0530

    Use axios/async/await

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import axios from 'axios';
 
 const App = () => {
     useEffect(
-        () => {
-            axios.get('https://jsonplaceholder.typicode.com/todos').then(response => console.table(response.data))
+        async () => {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+            console.table(response.data)
         }, []
     )
+
     return (
         <div>
             <h1>axios</h1>

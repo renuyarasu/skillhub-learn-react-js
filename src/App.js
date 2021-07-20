475864d82fb04ec4716e19a34072a3907ffbbc69
renukaprasad <renukaprasad.yarasu@innroad.com>
2021-07-20 13:35:45 +0530

    Use axios

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
-import React, { useReducer } from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
 
-const initialState = { count: 0 };
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'decrement':
-            return { count: state.count - 1 }
-        case 'increment':
-            return { count: state.count + 1 }
-        default:
-            throw new Error();
-    }
-}
-
-function App() {
-    const [state, dispatch] = useReducer(reducer, initialState);
+const App = () => {
+    useEffect(
+        () => {
+            axios.get('https://jsonplaceholder.typicode.com/todos').then(response => console.table(response.data))
+        }, []
+    )
     return (
         <div>
-            <strong>Count</strong>: {state.count} <br />
-            <button onClick={() => dispatch({ type: 'decrement' })}> - </button>
-            <button onClick={() => dispatch({ type: 'increment' })}> + </button>
-
+            <h1>axios</h1>
         </div>
     )
 }
